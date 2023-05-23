4c2c869678ba4ae2ce88271851ce63b49d1a8b9b
CANADAGAGOPA <kswms403@naver.com>
2023-05-23 17:41:56 +0900

    [second C]

---
+++ ThisIsJava/src/Chap02/Sec01/s01_2.java
@@ -0,0 +1,15 @@
+package Chap02.Sec01;
+
+public class s01_2 {
+    public static void main(String[] args) {
+        int x = 3;
+        int y = 5;
+        System.out.println("x : " + x + ", y : " + y);
+
+        int temp; // 빈 변수 선언
+        temp = x;
+        x = y;
+        y = temp;
+        System.out.println("x : " + x + ", y : " + y);
+    }
+}
