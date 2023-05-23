7ea9c2f36a71759d7dd484f0f55c4dfdc43f3f73
CANADAGAGOPA <kswms403@naver.com>
2023-05-23 19:22:32 +0900

    dffdf

---
+++ ThisIsJava/src/Chap02/Sec04/s04_1.java
@@ -0,0 +1,25 @@
+package Chap02.Sec04;
+
+public class s04_1 {
+    public static void main(String[] args) {
+        byte byteValue = 10;
+        int intValue = byteValue;
+        System.out.println("intValue : " + intValue);
+
+        char charValue = '가';
+        intValue = charValue;
+        System.out.println("가의 유니코드 : " + intValue);
+
+        intValue = 50;
+        long longValue = intValue;
+        System.out.println("longValue : " + longValue);
+
+        longValue = 100;
+        float floatValue = longValue;
+        System.out.println("floatValue : " + floatValue);
+
+        floatValue = 100.5F;
+        double doubleValue = floatValue;
+        System.out.println("doubleValue " + doubleValue);
+    }
+}
