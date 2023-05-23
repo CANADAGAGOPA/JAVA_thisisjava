97b3bd6c50fe7d77e34afd8ed808d0cd0968202c
CANADAGAGOPA <kswms403@naver.com>
2023-05-23 17:32:24 +0900

    [REFACTOR]

--- ThisIsJava/src/Main.java
+++ ThisIsJava/src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         int x = 10;
         int y=  10;
+        int z = 10;
         System.out.println("Hello world!");
     }
 }
