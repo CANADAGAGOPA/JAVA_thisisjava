dde59a8660bbc4a74bfd25c1d8cc79c29cc37b95
CANADAGAGOPA <kswms403@naver.com>
2023-05-25 20:10:23 +0900

    6

---
+++ ThisIsJava/src/Chap04/Sec01/s01_1.java
@@ -0,0 +1,7 @@
+package Chap04.Sec01;
+
+public class s01_1 {
+    public static void main(String[] args) {
+        
+    }
+}
--- ThisIsJava/src/Main.java
+++ ThisIsJava/src/Main.java
@@ -4,5 +4,13 @@ public class Main {
         int y=  10;
         int z = 10;
         System.out.println("Hello world!");
+
+        int a = x / 3;
+        int b = y % 3;
+        System.out.println(a);
+        System.out.println(b);
+
+        int value = 365;
+        System.out.println(value - (value % 100));
     }
 }
