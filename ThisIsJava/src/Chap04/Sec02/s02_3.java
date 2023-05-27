1b37a9e0f705f573cec08854350fdd5e6fc3d548
CANADAGAGOPA <kswms403@naver.com>
2023-05-27 23:08:10 +0900

    0527t

---
+++ ThisIsJava/src/Chap04/Sec02/s02_3.java
@@ -0,0 +1,20 @@
+package Chap04.Sec02;
+
+public class s02_3 {
+    public static void main(String[] args) {
+        char grade = 'B';
+
+        switch (grade) {
+            case 'A':
+            case 'a':
+                System.out.println("우수 회원입니다.");
+                break;
+            case 'B':
+            case 'b':
+                System.out.println("일반 회원입니다.");
+                break;
+            default:
+                System.out.println("손님입니다.");
+        }
+    }
+}
