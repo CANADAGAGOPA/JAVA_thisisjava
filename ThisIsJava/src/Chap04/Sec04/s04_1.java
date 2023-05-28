cd4f921d65f6b04195d39d731e223d49e2201cd8
CANADAGAGOPA <kswms403@naver.com>
2023-05-28 20:58:40 +0900

    0528s

---
+++ ThisIsJava/src/Chap04/Sec04/s04_1.java
@@ -0,0 +1,12 @@
+package Chap04.Sec04;
+
+public class s04_1 {
+    public static void main(String[] args) {
+        int i = 1;
+
+        while (i <= 10) {
+            System.out.println(i + " ");
+            i++;
+        }
+    }
+}
