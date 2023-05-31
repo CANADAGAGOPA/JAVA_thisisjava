00bda82e27cf53b68b9d75eb4516845d97705215
CANADAGAGOPA <kswms403@naver.com>
2023-05-31 20:16:53 +0900

    0531s

---
+++ ThisIsJava/src/Chap05/Sec02/s02_1.java
@@ -0,0 +1,11 @@
+package Chap05.Sec02;
+
+public class s02_1 {
+    public static void main(String[] args) {
+        int[] intArray = null;
+        //intArray[0] = 10; // NullPointerException
+
+        String str = null;
+        // System.out.println("총 문자 수 : " + str.length()); // NullPointerException
+    }
+}
