c219c12ca644d46dede9b328bd114527df88c04d
CANADAGAGOPA <kswms403@naver.com>
2023-06-01 19:26:38 +0900

    0601fff

---
+++ ThisIsJava/src/Chap05/Sec03/s03_6.java
@@ -0,0 +1,13 @@
+package Chap05.Sec03;
+
+public class s03_6 {
+    public static void main(String[] args) {
+        String ssn = "880815-1234567";
+
+        String firstNum = ssn.substring(0, 6);
+        System.out.println(firstNum);
+
+        String secondNum = ssn.substring(7);
+        System.out.println(secondNum);
+    }
+}
