4ffa369d369c666e2885e91a36b773aec33bed5b
CANADAGAGOPA <kswms403@naver.com>
2023-06-01 17:36:31 +0900

    0601s

---
+++ ThisIsJava/src/Chap05/Sec03/s03_2.java
@@ -0,0 +1,10 @@
+package Chap05.Sec03;
+
+public class s03_2 {
+    public static void main(String[] args) {
+        String hobby = "";
+        if(hobby.equals("")) {
+            System.out.println("hobby 변수가 참조는 String 객체는 빈 문자열");
+        }
+    }
+}
