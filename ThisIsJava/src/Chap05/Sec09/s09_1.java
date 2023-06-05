3099ba3c3a370e2ef60a7b1a775b51812689bb71
CANADAGAGOPA <kswms403@naver.com>
2023-06-05 12:16:19 +0900

    0605ff

---
+++ ThisIsJava/src/Chap05/Sec09/s09_1.java
@@ -0,0 +1,20 @@
+package Chap05.Sec09;
+
+public class s09_1 {
+    public static void main(String[] args) {
+        // 배열 변수 선언과 배열 생성
+        int[] scores = { 95, 71, 84, 93, 87 };
+
+        // 배열 항목 전체 합 구하기
+        int sum = 0;
+        for (int score : scores) {
+            sum = sum + score;
+        } // 5개의 항목이 한 번씩 score 변수에 저장되고 sum에 누적됨 (반복 횟수 : 5)
+
+        System.out.println("점수 총합 = " + sum);
+
+        // 배열 할목 전체 평균 구하기
+        double avg = (double) sum / scores.length;
+        System.out.println("점수 평균 = " + avg);
+    }
+}
