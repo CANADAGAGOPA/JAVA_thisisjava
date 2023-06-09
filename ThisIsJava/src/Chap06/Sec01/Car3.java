c223793b4f62101188ac2758e143c71967de7a07
CANADAGAGOPA <kswms403@naver.com>
2023-06-09 14:55:19 +0900

    0609f

---
+++ ThisIsJava/src/Chap06/Sec01/Car3.java
@@ -0,0 +1,7 @@
+package Chap06.Sec01;
+
+public class Car3 {
+    // 생성자 선언
+    Car3(String model, String color, int maxSpeed) {
+    }
+}
---
+++ ThisIsJava/src/Chap06/Sec01/CarExample3.java
@@ -0,0 +1,8 @@
+package Chap06.Sec01;
+
+public class CarExample3 {
+    public static void main(String[] args) {
+        Car3 myCar = new Car3("그랜저", "검정", 250);
+        // Car3 myCar = new Car(); // 기본 생성자 호출 못함
+    }
+}
