d18fb70e1aba706ef1b489e6352fa7429756a4ad
CANADAGAGOPA <kswms403@naver.com>
2023-06-09 15:48:21 +0900

    0609s

---
+++ ThisIsJava/src/Chap06/Sec01/Korean.java
@@ -0,0 +1,14 @@
+package Chap06.Sec01;
+
+public class Korean {
+    // 필드 선언
+    String nation = "대한민국";
+    String name;
+    String ssn;
+
+    // 생성자 선언
+    public Korean(String n, String s){
+        name = n; // 변수 name 초기화
+        ssn = s;  // 변수 ssn 초기화
+    }
+}
