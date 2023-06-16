c034bbcffa3a295a621680277baf4a69ba36c1b9
CANADAGAGOPA <kswms403@naver.com>
2023-06-16 17:21:31 +0900

    0616f

---
+++ ThisIsJava/src/Chap06/Sec04/Calculator.java
@@ -0,0 +1,13 @@
+package Chap06.Sec04;
+
+public class Calculator {
+    static double pi = 3.14159;
+
+    static int plus(int x, int y) {
+        return x + y;
+    }
+
+    static int minus(int x, int y) {
+        return x - y;
+    }
+}
