0cfdc5a8844eb3cbdaa1f221374f26edf26fcfea
CANADAGAGOPA <kswms403@naver.com>
2023-06-18 14:57:29 +0900

    0618s

---
+++ ThisIsJava/src/Chap06/Sec05/Earth.java
@@ -0,0 +1,13 @@
+package Chap06.Sec05;
+
+public class Earth {
+    // 상수 선언 및 초기화
+    static final double EARTH_RADIUS = 6400;
+
+    // 상수 선언
+    static final double EARTH_SURFACE_AREA;
+
+    static {
+        EARTH_SURFACE_AREA = 4 * Math.PI * EARTH_RADIUS * EARTH_RADIUS;
+    }
+}
