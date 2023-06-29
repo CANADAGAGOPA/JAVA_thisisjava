e251c41b5f5f6d627059659156a59ca257a10aa0
CANADAGAGOPA <kswms403@naver.com>
2023-06-29 15:17:58 +0900

    0629f

--- ThisIsJava/src/Chap06/Sec06/hyundai/Car.java
+++ ThisIsJava/src/Chap06/Sec06/hyundai/Car.java
@@ -3,6 +3,7 @@ package Chap06.Sec06.hyundai;
 // import 문으로 다른 패키지 클래스 사용을 명시
 import Chap06.Sec06.hankook.SnowTire;
 import Chap06.Sec06.kumho.AllSeasonTire;
+
 public class Car {
     // 부품 필드 선언
     Chap06.Sec06.hankook.Tire tire1 =  new Chap06.Sec06.hankook.Tire();
